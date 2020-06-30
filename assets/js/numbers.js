28b88872851dbf2fcc18c87b292df340472be788
CJ Fulton <fulchr3356@gmail.com>
2020-06-30 00:05:10 -0400

    Work in progress without eval

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,3 +1,14 @@
 function compute(expression) {
     // TODO - write method definition here
+     var operations = ["-","+","*", "/"];
+     var tempString = "";
+     for (var i = 0; i < expression.length;i++){
+        if (operations.includes(expression.charAt(i))){
+            tempString = "0";
+        }
+     }
+     return eval(expression);
 }
+
+
+
